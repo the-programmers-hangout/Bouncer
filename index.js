07e0d932c627ac29045402266f9357464a9b9eac
moe <moe9999@protonmail.com>
2021-05-10 20:58:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  const regex = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|club)|discordapp\.com\/invite|discord\.com\/invite)\/.+[a-z]/gi;
+
+  const allowedInvites = process.env.INVITES.split(',');
+
+  let content = message.content;
+  allowedInvites.forEach((invite) => {
+    content = content.replace(invite, '');
+  });
+
+
+  if (!message.member.hasPermission('MANAGE_GUILD')) { 
+    if (regex.exec(content)) {
+      await message.delete();
+
+      const alertChannel = client.channels.cache.get(process.env.ALERT_CHAN_ID);
+      alertChannel.send(`
+        **<@${message.author.id}> sent an invite in <#${message.channel.id}>**\nMessage content:
+      `);
+
+      alertChannel.send(message.content);
+    }
+} 
+});
+
+client.login(process.env.TOKEN);
